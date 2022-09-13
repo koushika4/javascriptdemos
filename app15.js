05c0275bb318972e6674fc5f71e1413f447c778c
koushikabalusu <koushika1204@sasi.ac.in>
2022-09-13 13:08:08 +0530

    changes

---
+++ app15.js
@@ -0,0 +1,12 @@
+// arguments and spread
+
+function greet(firstname, lastname, ...others) {
+    console.log('Welcome ' + firstname + ' ' + lastname);
+    console.log(others);
+  }
+  
+  greet('John', 'Doe');
+  greet('John');
+  greet('John', 'Doe', 'hello', 'how', 'are', 'you');
+  greet('John', 'Doe', 'hello', 'how');
+  
